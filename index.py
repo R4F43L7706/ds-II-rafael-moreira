c8b61105cb82f1f7dc5b90c4d8e41e278e2a9318
R4F43l7706 <rafaalves.rham@gmail.com>
2025-02-18 20:26:08 -0300

    DS 2

---
+++ index.py
@@ -0,0 +1,10 @@
+#print("Hello")
+#a = input('Digite um numero:')
+#b = input('Digite um numero:')
+#print (a, 'e', b)
+
+
+a = int(input('Digite um numero:'))
+b = int(input('Digite um numero:'))
+z = a+b
+print(z)
